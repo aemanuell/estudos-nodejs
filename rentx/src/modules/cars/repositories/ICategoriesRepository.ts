ae7f0c7d56fa56e76474cd167bcc9f876f240a71
aemanuell <antonioemanueell@gmail.com>
2022-12-28 15:05:31 -0300

    creating postgres container

---
+++ rentx/src/modules/cars/repositories/ICategoriesRepository.ts
@@ -0,0 +1,15 @@
+import { Category } from "../model/Category";
+
+
+interface ICreateCategoryDTO {
+    name: String;
+    description: String;
+}
+
+interface ICategoriesRepository {
+    findByName(name: String): Category;
+    list(): Category[];
+    create({name, description}: ICreateCategoryDTO): void;
+}
+
+export { ICategoriesRepository, ICreateCategoryDTO}
